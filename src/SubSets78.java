02bd6613481dc82cafd16a58ad868ac3c133e4af
anishsaha <anishsaha_bec@yahoo.com>
2024-07-08 19:52:26 -0700

    refactor

--- src/SubSets78.java
+++ src/SubSets78.java
@@ -2,23 +2,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Given a Set of Integers , print all subsets
-public class SubSets {
+public class SubSets78 {
     public static int numcalls;
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
+        int[] intArray = new int[]{ 1,3,5};
 
-        List<Integer> choices = new ArrayList<>();
-        choices.add(1);
-        choices.add(3);
-        choices.add(5);
+       // choices.add(1);
+       // choices.add(3);
+       // choices.add(5);
+
+       // subListHelper (choices,chosen,resultSets);
+       // System.out.println(resultSets);
+        System.out.println("Final result"+ subsets(intArray) );
+
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> resultSets = new ArrayList<>();
-        List<Integer> chosen = new ArrayList<>();
+        List<Integer> chosen = new ArrayList<Integer>();
+        List<Integer> choices = new ArrayList<Integer>();
+
+
+        for (int i : nums)
+        {
+            choices.add(i);
+        }
         subListHelper (choices,chosen,resultSets);
-        System.out.println(resultSets);
         System.out.println("numcalls"+ numcalls );
 
+        return resultSets;
     }
 
     private static void subListHelper(List<Integer> choices,List<Integer> chosen, List<List<Integer>> resultSets) {
