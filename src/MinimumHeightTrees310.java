2c4daa042d9fe61208b5b6e4e8cbee0905cad439
anishsaha <anishsaha_bec@yahoo.com>
2024-08-03 15:22:28 -0700

    65 of 71 leetcode passed

--- src/MinimumHeightTrees310.java
+++ src/MinimumHeightTrees310.java
@@ -4,23 +4,27 @@ public class MinimumHeightTrees310 {
 
     public static void main(String[] args) {
 
-       /* int[] intArray1 = new int[]{ 1,0};
+        int[] intArray1 = new int[]{ 1,0};
         int[] intArray2 = new int[]{ 1,2};
         int[] intArray3 = new int[]{ 1,3};
 
-        int[][] points  = new int[][]{intArray1,intArray2,intArray3};*/
+        int[][] points  = new int[][]{intArray1,intArray2,intArray3};
 
 
-        int[] intArray1 = new int[]{ 3,0};
+        /*int[] intArray1 = new int[]{ 3,0};
         int[] intArray2 = new int[]{ 3,1};
         int[] intArray3 = new int[]{ 3,2};
         int[] intArray4 = new int[]{ 3,4};
         int[] intArray5 = new int[]{ 5,4};
         int[][] points  = new int[][]{intArray1,intArray2,intArray3,intArray4,intArray5};
+*/
+
+       // int[][] points= new int[][] {{0,1},{0,2},{0,3},{3,4},{4,5}};
+       // int[][] points= new int[][] {{0,1},{1,2},{1,3},{2,4},{3,5},{4,6}};
 
 
-       // System.out.println( findMinHeightTrees(4,points));
-         System.out.println( findMinHeightTrees(6,points));
+        System.out.println( findMinHeightTrees(4,points));
+        // System.out.println( findMinHeightTrees(6,points));
     }
 
     public static List<Integer> findMinHeightTrees(int n, int[][] edges) {
@@ -42,31 +46,37 @@ public class MinimumHeightTrees310 {
         }
         List<Integer> res = new ArrayList<>();
         int[] maxHeights = new int[n];
+        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
+       // treeMap.put(Integer.MAX_VALUE,new ArrayList<>());
         for (int i=0; i<n; i++) {
             Set<Integer> visited = new HashSet<>();
             List<Integer> chosenPath = new ArrayList<>();
             List<List<Integer>> allPathsFromNode = new ArrayList<>();
             maxHeights[i] = Integer.MIN_VALUE;
             int startNode=i;
-            backTrackingDfs(i,startNode,visited,chosenPath,allPathsFromNode,graphMap,maxHeights);
+            int minSoFar = Integer.MAX_VALUE;
+            if(i>0) {
+                minSoFar=treeMap.firstKey();
+            }
+            backTrackingDfs(i,startNode,visited,chosenPath,allPathsFromNode,graphMap,maxHeights,minSoFar);
             //  System.out.println("allPathsFromNode"+allPathsFromNode);
+           // if(maxHeights[i]>Integer.MIN_VALUE) {
+                List<Integer> nodes = treeMap.getOrDefault(maxHeights[i],new ArrayList<>());
+                nodes.add(i);
+                treeMap.put(maxHeights[i],nodes);
+           // }
 
         }
-        // System.out.println("maxHeights"+Arrays.toString(maxHeights));
-        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
-        for (int i=0; i<n; i++) {
-            List<Integer> nodes = treeMap.getOrDefault(maxHeights[i], new ArrayList<>());
-            nodes.add(i);
-            treeMap.put(maxHeights[i], nodes);
-        }
-        // System.out.println("treeMap"+treeMap);
+         System.out.println("maxHeights"+Arrays.toString(maxHeights));
+
+         System.out.println("treeMap"+treeMap);
         int firstEntry = treeMap.firstKey();
         List<Integer> result = treeMap.get(firstEntry);
         return result;
     }
 
-    private static  void backTrackingDfs(int source, int startNode, Set<Integer> visited, List<Integer> chosenPath, List<List<Integer>> allPathsFromNode, Map<Integer, List<Integer>> graphMap, int[] maxHeights) {
-
+    private static  void backTrackingDfs(int source, int startNode, Set<Integer> visited, List<Integer> chosenPath, List<List<Integer>> allPathsFromNode, Map<Integer, List<Integer>> graphMap, int[] maxHeights, Integer minSoFar) {
+     System.out.println("for startNode "+startNode+" minSofar "+minSoFar);
         chosenPath.add(source);
         visited.add(source);
         List<Integer> eligibleNeighbours = new ArrayList<>();
@@ -78,19 +88,28 @@ public class MinimumHeightTrees310 {
         }
         if (eligibleNeighbours.isEmpty()) {
             //found leaf node
-            // System.out.println(chosenPath);
+            System.out.println("chosenPath"+chosenPath);
             allPathsFromNode.add(new ArrayList<>(chosenPath));
             if(chosenPath.size() > maxHeights[startNode]) {
                 maxHeights[startNode] = chosenPath.size();
             }
         }
 
-        //explore only if maxHeight has not reached the size of graph, because in a tree max edge can not be more than number of nodes.
-        if( maxHeights[startNode]<graphMap.size() ) {
+
+        if( (maxHeights[startNode]>0 && maxHeights[startNode] <= minSoFar) || (maxHeights[startNode]<0) ) {
+            System.out.println(" minSoFar "+minSoFar+" chosenPath.size() "+chosenPath.size());
             for (Integer neighbour : eligibleNeighbours) {
-                backTrackingDfs(neighbour, startNode, visited,chosenPath,allPathsFromNode,graphMap,maxHeights);
-            }}
+                backTrackingDfs(neighbour, startNode, visited,chosenPath,allPathsFromNode,graphMap,maxHeights, minSoFar);
+            }
+    }
 
         chosenPath.remove(chosenPath.size()-1);
 
-    }}
+    }
+
+
+}
+
+
+
+
