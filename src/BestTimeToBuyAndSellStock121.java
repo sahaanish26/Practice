553f4fef622c9e04f074aa57cb8f3166e6a18837
anishsaha <anishsaha_bec@yahoo.com>
2024-09-20 13:51:51 -0700

    all cases passed

--- src/BestTimeToBuyAndSellStock121.java
+++ src/BestTimeToBuyAndSellStock121.java
@@ -4,11 +4,11 @@ public class BestTimeToBuyAndSellStock121 {
     public static void main (String[ ] args){
      
         int[] prices = {7,1,5,3,6,4};
-        System.out.println("mxProfit"+maxProfit(prices));
+        System.out.println("mxProfit1"+maxProfit(prices));
 
     }
     
-    
+    // can be solved by min so far concept
     public static int maxProfit(int[] prices) {
         
         int [] minSofar = new int[prices.length];
@@ -29,6 +29,34 @@ public class BestTimeToBuyAndSellStock121 {
         }
 
         System.out.println(Arrays.toString(minSofar));
+
+        if (max>=0)
+        return max;
+
+        return 0;
+    }
+    //with Max so far on right side
+    public static int maxProfit1(int[] prices) {
+        
+        int [] maxSofar = new int[prices.length];
+        int max = Integer.MIN_VALUE;
+
+        for (int i= prices.length-1; i>=0;i--){
+            if(i==prices.length-1){maxSofar[i]=prices[i];}
+            else{
+                maxSofar[i] = Math.max(maxSofar[i+1],prices[i]);
+
+            }
+           
+        }
+        System.out.println(Arrays.toString(maxSofar));
+
+        for (int i= prices.length-1; i>=0;i--){
+          max = Math.max(max,maxSofar[i]-prices[i]);
+
+        }
+
+       
 
         if (max>=0)
         return max;
