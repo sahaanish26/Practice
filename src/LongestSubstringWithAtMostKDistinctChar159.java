42eb4e3755e1c9c3e8643974ecddbd94e570e01d
anishsaha <anishsaha_bec@yahoo.com>
2024-09-21 23:18:20 -0700

    sliding window

---
+++ src/LongestSubstringWithAtMostKDistinctChar159.java
@@ -0,0 +1,87 @@
+import java.util.*;
+public class LongestSubstringWithAtMostKDistinctChar159{
+
+
+    public static void main(String[] args){
+     String s = "eceba";
+     int k =2;
+     //int result  = LongestSubstring(s,k);
+
+     //System.out.println("result is  "+result);
+
+     int result1  = MinSubstring(s,k);
+     System.out.println("result1 is  "+result1);
+
+
+    }
+
+    public static int LongestSubstring(String s , int K){
+
+     int leftP=0;
+     int rightP=0;
+     Map<Character,Integer> characterCountMap = new HashMap<>();
+     int max = Integer.MIN_VALUE;
+
+     while(rightP<s.length()){
+
+        Character cRightP = s.charAt(rightP);
+        characterCountMap.put(cRightP,characterCountMap.getOrDefault(cRightP,0)+1);
+    
+        rightP++;
+
+        while(characterCountMap.size()>K){
+            Character cLeftP = s.charAt(leftP);
+            characterCountMap.put(cLeftP,characterCountMap.getOrDefault(cLeftP, 0)-1);
+            if(characterCountMap.get(cLeftP)<=0){
+                characterCountMap.remove(cLeftP);
+
+            }
+            leftP++;
+
+        }
+        max = Math.max(max,rightP-leftP);
+       
+
+     }
+
+
+        return max;
+    }
+
+
+    public static int MinSubstring(String s , int K){
+
+        int leftP=0;
+        int rightP=0;
+        Map<Character,Integer> characterCountMap = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+   
+        while(rightP<s.length()){
+   
+           Character cRightP = s.charAt(rightP);
+           characterCountMap.put(cRightP,characterCountMap.getOrDefault(cRightP,0)+1);
+       
+           rightP++;
+   
+           while(characterCountMap.size()<=2 &&  characterCountMap.size()!=0 ){
+            System.out.println("leftP "  +leftP+"rightP "+ rightP);
+               min = Math.min(min, rightP-leftP);
+               System.out.println("min " +min);
+               Character cLeftP = s.charAt(leftP);
+               characterCountMap.put(cLeftP,characterCountMap.getOrDefault(cLeftP, 0)-1);
+               if(characterCountMap.get(cLeftP)<=0){
+                   characterCountMap.remove(cLeftP);
+   
+               }
+               leftP++;
+   
+           }
+          // max = Math.max(max,rightP-leftP);
+          
+   
+        }
+   
+   
+           return min;
+       }
+}
